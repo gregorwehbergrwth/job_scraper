b7765944384dd1db77f3e5956169f83b45eb8b8d
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2026-01-22 20:23:52 +0000

    minor mistake

--- functions/frequency.py
+++ functions/frequency.py
@@ -8,6 +8,6 @@ def check(mouse, log, problem_log):
 
     time_difference = (time.mktime(now) - time.mktime(last_checked)) / 3600
 
-    frequency_hours = {"fourminutely": "1/15", "fiveminutely": "1/12", "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 1000}
+    frequency_hours = {"fourminutely": 1/15, "fiveminutely": 1/12, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 1000}
 
     return time_difference + 1 >= frequency_hours.get(log["frequency"], None) and mouse not in problem_log.keys()
