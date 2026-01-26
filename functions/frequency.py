1508b8e549925ba457d146a3811f02f0ade87d8b
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2026-01-26 20:50:20 +0000

    change frequency calculation

--- functions/frequency.py
+++ functions/frequency.py
@@ -1,13 +1,13 @@
 import time
 
 
-def check(mouse, log, problem_log, now):
+def check(mouse, log, problem_log, now, runmode):
+    if (runmode == "job" and mouse == "wg_gesucht") or (runmode == "wohnung" and mouse != "wg_gesucht"):
+        return False
 
-    now = time.strptime(now, "%Y-%m-%d %H:%M:%S")
-    last_checked = time.strptime(log["last_checked"], "%Y-%m-%d %H:%M:%S")
+    to_time = lambda x: time.mktime(time.strptime(x, "%Y-%m-%d %H:%M:%S"))
+    time_difference = (to_time(now) - to_time(log["last_checked"])) / 3600
 
-    time_difference = (time.mktime(now) - time.mktime(last_checked)) / 3600
-
-    frequency_hours = {"fourminutely": 1/15, "fiveminutely": 1/12, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
+    frequency_hours = {"instant": 1, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
 
     return time_difference + 1 >= frequency_hours.get(log["frequency"], None) and mouse not in problem_log.keys()
