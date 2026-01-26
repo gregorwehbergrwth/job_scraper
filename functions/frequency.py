cdf32d445646008c4329c05f54849a3d4a390a75
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2026-01-26 21:31:14 +0000

    fixed mistake

--- functions/frequency.py
+++ functions/frequency.py
@@ -8,6 +8,6 @@ def check(mouse, log, problem_log, now, runmode):
     to_time = lambda x: time.mktime(time.strptime(x, "%Y-%m-%d %H:%M:%S"))
     time_difference = (to_time(now) - to_time(log["last_checked"])) / 3600
 
-    frequency_hours = {"instant": 1, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
+    frequency_hours = {"instant": 1/100, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
 
     return time_difference + 1 >= frequency_hours.get(log["frequency"], None) and mouse not in problem_log.keys()
