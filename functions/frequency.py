c2ddadb46fb10ed2f7c302cb15001b4dae1599f3
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2026-01-28 15:48:01 +0000

    rewrite for clarity

--- functions/frequency.py
+++ functions/frequency.py
@@ -2,12 +2,24 @@ import time
 
 
 def check(mouse, log, problem_log, now, runmode):
-    if (runmode == "job" and mouse == "wg_gesucht") or (runmode == "wohnung" and mouse != "wg_gesucht"):
-        return False
-
     to_time = lambda x: time.mktime(time.strptime(x, "%Y-%m-%d %H:%M:%S"))
     time_difference = (to_time(now) - to_time(log["last_checked"])) / 3600
 
-    frequency_hours = {"instant": 1/100, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
+    frequency_hours = {"instant": 1 / 100, "hourly": 1, "daily": 24, "weekly": 24 * 7, "never": 10000}
 
-    return time_difference + 1 >= frequency_hours.get(log["frequency"], None) and mouse not in problem_log.keys()
+    print(f"Checking frequency for {mouse} in {runmode}")
+    if runmode == "job" and mouse == "wg_gesucht":
+        print(f"blocked {mouse} in {runmode} mode because no wg_gesucht allowed")
+        return False
+    elif runmode == "wohnung" and mouse != "wg_gesucht":
+        print(f"blocked {mouse} in {runmode} mode because only wg_gesucht allowed")
+        return False
+    elif mouse in problem_log.keys():
+        print(f"blocked {mouse} because it is in problem log")
+        return False
+    elif time_difference <= frequency_hours.get(log["frequency"], None):
+        print(f"blocked {mouse} because frequency limit not reached: {time_difference} hours since last check, needs {frequency_hours.get(log['frequency'], None)} hours")
+        return False
+    else:
+        print(f"allowed {mouse} to run: {time_difference} hours since last check, needs {frequency_hours.get(log['frequency'], None)} hours")
+        return True
