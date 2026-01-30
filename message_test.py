7456c87a4cab95552cda39e892f984dc8e8687e4
gregorwehbergrwth <162116009+gregorwehbergrwth@users.noreply.github.com>
2026-01-30 15:33:50 +0000

    temp save

--- message_test.py
+++ message_test.py
@@ -1,13 +1,10 @@
 import time
 import asyncio
-import json
-import asyncio
 from telegram import Bot
-from telegram.error import BadRequest
-import re
+from telegram.error import BadRequest, TimedOut, NetworkError
 
 api_key = '8030882097:AAHyDEN1DWhyRYUhbUOBA8b-Gz0AIpOEJlg'
-user_ids = ['5623557325']
+user_id = '5623557325'
 _bot = None
 
 
@@ -15,9 +12,6 @@ def problem(mouse, error, send_message=True):
     print(f"Problem in {mouse}: {error}")
 
 
-
-
-semaphore = asyncio.Semaphore(5)
 _bot = None
 
 
@@ -30,27 +24,31 @@ def get_bot():
     return _bot
 
 
-def messages(texte, test=False):
-
-    async def send_one(user_id, text):
-        async with semaphore:
-            await _bot.send_message(chat_id=user_id, text=text)
-
-    async def send_message_async(texts):
-        _bot = get_bot()
-        tasks = [send_one(user_id, text) for text in texts for user_id in user_ids]
-        try:
-            await asyncio.gather(*tasks)
-        except BadRequest as e:
-            print(f"Telegram API Error: {e}")
-
+def send_message(text, bot):
     try:
-        if not test and texte:
-            asyncio.run(send_message_async(texte))
+        bot.send_message(chat_id=user_id, text=text)
+        return True
+    except (BadRequest, TimedOut, NetworkError) as e:
+        problem(mouse="send_message", error=f"Error sending to {user_id}: {e}")
+        return False
+
+
+def messages(texte, test=False):
+    _bot = get_bot()
+    try:
+        unsend_messages = []
+        if not test:
+            for message in texte:
+                if send_message(message, _bot):
+                    pass
+                else:
+                    unsend_messages.append(message)
+        if unsend_messages:
+            messages(texte)
     except Exception as e:
-        problem(mouse="message", error=f"Error sending message: {e}", send_message=False)
+        problem(mouse="messages", error=f"Critical error: {e}")
     finally:
-        print(*texte, sep="\n")
+        print(*texte, sep="\n") if texte else print("No new messages to send.")
 
 
 
